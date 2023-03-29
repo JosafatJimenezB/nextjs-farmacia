e04a169be3e398772bc1b3a53306832c578f1d5b
josafat <josa221001@gmail.com>
2023-03-28 22:16:05 -0600

    add: backend del login

---
+++ pages/api/auth/login.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { serialize } from "cookie";
+
+export default async function loginHandler(req, res) {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+    const data = { email, password };
+
+    try {
+      const response = await axios.post(
+        "https://farmacia-api-production.up.railway.app/auth/authenticate",
+        data
+      );
+
+      const token = response.data.replace(/%22/g, "");
+
+      const serialized = serialize("UserToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV == "production",
+        sameSite: "none",
+        maxAge: 60 * 60 * 24, // 1 day
+        path: "/",
+      });
+      res.setHeader("Set-Cookie", serialized);
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: "Error al enviar datos a la api externa" });
+    }
+  } else {
+    res.status(401).json({ message: "Metodo HTTP no permitido" });
+  }
+}

3e793b217a59b8b2749c7a498fa98e33a49bcfc2
josafat <josa221001@gmail.com>
2023-03-30 11:55:21 -0600

    fix: excepcion de cross-site al guardar token

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -13,27 +13,26 @@ export default async function loginHandler(req, res) {
         data
       );
 
-      const token = response.data.replace(/%22/g, "");
+      const token = response.data;
+
+      // const token = response.data.replace(/%22/g, "");
 
       const serialized = serialize("UserToken", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV == "production",
-        sameSite: "none",
+        sameSite: "strict",
         maxAge: 60 * 60 * 24, // 1 day
         path: "/",
       });
       res.setHeader("Set-Cookie", serialized);
       res.status(200).json(response.data);
-      toast.success("Bienvenido");
     } catch (error) {
       console.log(error);
       res
         .status(500)
         .json({ message: "Error al enviar datos a la api externa" });
-      toast.error("Datos incorrectos");
     }
   } else {
     res.status(401).json({ message: "Metodo HTTP no permitido" });
-    toast.error("Metodo HTTP no permitido");
   }
 }

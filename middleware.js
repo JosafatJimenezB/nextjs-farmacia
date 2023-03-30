bf2eacb93ef1df47f20f0a1de2a496aeee1c5221
josafat <josa221001@gmail.com>
2023-03-30 12:09:39 -0600

    add: redireccionamiento con token

--- middleware.js
+++ middleware.js
@@ -3,4 +3,6 @@ import { NextResponse } from "next/server";
 export async function middleware(request) {
   const token = request.cookies.get("UserToken");
   console.log(token);
+
+  // if (!token) return NextResponse.redirect(new URL("/login", request.url));
 }

3a04993a388ae5ddfd7a62cf3c169e7e6470c058
josafat <josa221001@gmail.com>
2023-03-30 13:03:32 -0600

    styles: estilos del button y los container

--- pages/login.js
+++ pages/login.js
@@ -23,22 +23,48 @@ function Login() {
 
   return (
     <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-      <div className="container w-3 h-4">
+      <div className="w-full max-w-md space-y-8">
         <form onSubmit={handleSubmit} className="mt-8 space-y-6">
-          <input
-            name="email"
-            type="email"
-            placeholder="email"
-            className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus: z-1 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            onChange={handleChange}
-          ></input>
-          <input
-            name="password"
-            type="password"
-            placeholder="password"
-            onChange={handleChange}
-          ></input>
-          <button className="bg-blue-400">Login</button>
+          <div>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus: z-1 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div>
+            <input
+              name="password"
+              type="password"
+              className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="password"
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div>
+            <button
+              type="submit"
+              class="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg
+                  class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+              Login
+            </button>
+          </div>
         </form>
       </div>
     </div>

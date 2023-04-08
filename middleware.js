665f8ef57fd87a1a9448ea25446dc3ecc93783e3
josafat <josa221001@gmail.com>
2023-04-08 16:50:05 -0600

    add: validando si existe un token y proteccion de las rutas

--- middleware.js
+++ middleware.js
@@ -2,7 +2,16 @@ import { NextResponse } from "next/server";
 
 export async function middleware(request) {
   const token = request.cookies.get("UserToken");
-  // console.log(token);
 
-  // if (!token) return NextResponse.redirect(new URL("/login", request.url));
+  const session = await token;
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/", "/dashboard"],
+};

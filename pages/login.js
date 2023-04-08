b2259424ed7671bc8fb9953ae0df0ab88aa1beff
josafat <josa221001@gmail.com>
2023-04-08 16:52:07 -0600

    fix: reparando class por className

--- pages/login.js
+++ pages/login.js
@@ -46,11 +46,11 @@ function Login() {
           <div>
             <button
               type="submit"
-              class="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 <svg
-                  class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"

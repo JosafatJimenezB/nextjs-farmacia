cbdb877e25186714827fdea2c989cc0841cbe88c
josafat <josa221001@gmail.com>
2023-04-08 18:18:28 -0600

    add: añadiendo boton de cerrar sesion

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,24 @@
-function dashboard() {
+import axios from "axios";
+import { useRouter } from "next/router";
+
+function Dashboard() {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await axios.post("/api/auth/logout");
+      router.push("/login");
+    } catch (err) {
+      router.push("/login");
+    }
+  };
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <button onClick={() => logout()}>Salir</button>
     </div>
   );
 }
 
-export default dashboard;
+export default Dashboard;

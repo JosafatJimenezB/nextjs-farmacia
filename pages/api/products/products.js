e65d3aef1e60e4c4e264b2cdcae68143974a0ec8
josafat <josa221001@gmail.com>
2023-04-12 20:26:29 -0600

    add: agregando try catch de los productos

--- pages/api/products/products.js
+++ pages/api/products/products.js
@@ -3,14 +3,18 @@ import axios from "axios";
 export default async function productsHandler(req, res) {
   const { UserToken } = await req.cookies;
 
-  const instance = axios.create({
-    baseURL: "https://farmacia-api-production.up.railway.app",
-    headers: {
-      Authorization: `Bearer ${UserToken}`,
-      "Content-Type": "applicaton/json",
-    },
-  });
-  const products = await instance.get("/products");
+  try {
+    const instance = axios.create({
+      baseURL: "https://farmacia-api-production.up.railway.app",
+      headers: {
+        Authorization: `Bearer ${UserToken}`,
+        "Content-Type": "applicaton/json",
+      },
+    });
+    const products = await instance.get("/products");
 
-  res.status(200).json(products.data);
+    res.status(200).json(products.data);
+  } catch (error) {
+    console.log("No se encontraron productos");
+  }
 }

6a74521a654b858d77a3ed2f7f49ea36e658ec84
josafat <josa221001@gmail.com>
2023-04-12 20:27:55 -0600

    add: reajustando dashboard en componentes y agregando skeleton a productos

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,29 +1,9 @@
-import axios from "axios";
-import { useRouter } from "next/router";
-import { Toaster, toast } from "react-hot-toast";
-import Products from "./components/Products/Products";
+import Menu from "@/components/DashboardMenu/DashboardMenu";
 
-function Dashboard(products) {
-  const router = useRouter();
-
-  const logout = async () => {
-    try {
-      await axios.post("/api/auth/logout");
-      router.push("/login");
-      toast.success("Sesión cerrada con éxito");
-    } catch (err) {
-      router.push("/login");
-    }
-  };
-
+function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
-      <button onClick={() => logout()}>Salir</button>
-      <Toaster />
-      <div>
-        <Products />
-      </div>
+      <Menu />
     </div>
   );
 }

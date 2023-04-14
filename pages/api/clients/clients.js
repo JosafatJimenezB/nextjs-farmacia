d69297a4d4c09f8454e8a6815204008c96fc4203
josafat <josa221001@gmail.com>
2023-04-13 21:41:27 -0600

    add: endpoints para peticion de datos

---
+++ pages/api/clients/clients.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default async function productsHandler(req, res) {
+  const { UserToken } = await req.cookies;
+
+  try {
+    const instance = axios.create({
+      baseURL: "https://farmacia-api-production.up.railway.app",
+      headers: {
+        Authorization: `Bearer ${UserToken}`,
+        "Content-Type": "applicaton/json",
+      },
+    });
+    const products = await instance.get("/clients");
+
+    res.status(200).json(products.data);
+  } catch (error) {
+    console.log("No se encontraron productos");
+  }
+}

fbaeaf6176df3ce6c3454104fbb12f829ee634fd
josafat <josa221001@gmail.com>
2023-06-04 19:47:44 -0600

    updt: agregando nuevo producto

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import NewProduct from "./NewProduct";
 import ProductSkeleton from "../Skeleton/Skeleton";
 import Notfound from "../Skeleton/Notfound";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -23,40 +24,43 @@ const Products = () => {
   }, []);
 
   return (
-    <div>
-      <h2 className="mb-10 text-2xl text-start text-teal-500">Productos</h2>
-      <table className="text-center min-w-full mx-auto border-2 border-teal-700">
-        <tr className="border-2 border-teal-700">
-          <th>Id</th>
-          <th>Nombre</th>
-          <th>Descripción</th>
-          <th>Precio</th>
-          <th>Cantidad</th>
-        </tr>
-        <tbody className="px-2">
-          {loading ? (
-            <>
-              <ProductSkeleton />
-              <ProductSkeleton animation="wave" />
-              <ProductSkeleton animation="wave" />
-              <ProductSkeleton animation="wave" />
-              <ProductSkeleton animation={false} />
-            </>
-          ) : (
-            products.map((product, index) => (
-              <tr key={index}>
-                <td className="px-2 py-2">{product.id}</td>
-                <td className="px-2 py-2">{product.name}</td>
-                <td className="px-2 py-2">{product.description}</td>
-                <td className="px-2 py-2 text-center">{product.price}</td>
-                <td className="px-2 py-2 text-center">{product.quantity}</td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </table>
-      {products.length === 0 && !loading && <Notfound />}
-    </div>
+    <>
+      <div>
+        <h2 className="mb-10 text-2xl text-start text-teal-500">Productos</h2>
+        <table className="text-center min-w-full mx-auto border-2 border-teal-700">
+          <tr className="border-2 border-teal-700">
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+          </tr>
+          <tbody className="px-2">
+            {loading ? (
+              <>
+                <ProductSkeleton />
+                <ProductSkeleton animation="wave" />
+                <ProductSkeleton animation="wave" />
+                <ProductSkeleton animation="wave" />
+                <ProductSkeleton animation={false} />
+              </>
+            ) : (
+              products.map((product, index) => (
+                <tr key={index}>
+                  <td className="px-2 py-2">{product.id}</td>
+                  <td className="px-2 py-2">{product.name}</td>
+                  <td className="px-2 py-2">{product.description}</td>
+                  <td className="px-2 py-2 text-center">{product.price}</td>
+                  <td className="px-2 py-2 text-center">{product.quantity}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+        {products.length === 0 && !loading && <Notfound />}
+      </div>
+      <NewProduct />
+    </>
   );
 };
 
